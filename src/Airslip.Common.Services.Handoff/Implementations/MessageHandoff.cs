aff86ada3a008a53c5d865ffde16250864da6ec9
Graham Whitehouse <83427700+gwairslip@users.noreply.github.com>
2022-01-07 15:07:18 +0000

    Removed old code

--- src/Airslip.Common.Services.Handoff/Implementations/MessageHandoff.cs
+++
@@ -1,8 +0,0 @@
-namespace Airslip.Common.Services.Handoff.Implementations;
-
-public class MessageHandoff
-{
-    public Type HandlerType { get; set; }
-
-    public string QueueName { get; set; }
-}
--- src/Airslip.Common.Services.Handoff/Interfaces/IMessageHandoffWorker.cs
+++
@@ -1,6 +0,0 @@
-namespace Airslip.Common.Services.Handoff.Interfaces;
-
-public interface IMessageHandoffWorker
-{
-    Task Execute(string message);
-}
