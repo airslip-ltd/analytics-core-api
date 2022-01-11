42fb709aa1bf7a387d10d26ab9964bfb656c72b2
Graham Whitehouse <83427700+gwairslip@users.noreply.github.com>
2022-01-11 13:34:11 +0000

    Updated deployment

--- env/outputs.tf
+++ env/outputs.tf
@@ -5,3 +5,7 @@ output "resource_group_name" {
 output "function_app_names" {
   value = module.func_app_host.function_app_names
 }
+
+output "database_connection_string" {
+  value = module.sql_server.connection_string
+}
