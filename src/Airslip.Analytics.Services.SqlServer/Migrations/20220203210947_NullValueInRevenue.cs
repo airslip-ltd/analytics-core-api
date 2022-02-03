9b4031325d533aed9e44edde3cf0850c0f984dfc
Graham Whitehouse <83427700+gwairslip@users.noreply.github.com>
2022-02-03 21:11:18 +0000

    Fix issue in null value handling

---
+++ src/Airslip.Analytics.Services.SqlServer/Migrations/20220203210947_NullValueInRevenue.cs
@@ -0,0 +1,20 @@
+﻿using Airslip.Analytics.Services.SqlServer.Extensions;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Airslip.Analytics.Services.SqlServer.Migrations
+{
+    public partial class NullValueInRevenue : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddSqlFiles(nameof(NullValueInRevenue));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
