95326ac777d862de560e70194e10c7640728b5a3
Graham Whitehouse <83427700+gwairslip@users.noreply.github.com>
2022-02-03 20:18:36 +0000

    Fixed support for negative balances

---
+++ src/Airslip.Analytics.Services.SqlServer/Database/NegativeBalances/Procedures/dbo.CreateAccountBalanceSnapshot.prc.sql
@@ -0,0 +1,18 @@
+create or alter proc dbo.CreateAccountBalanceSnapshot
+(
+    @EntityId as varchar(33),
+    @AirslipUserType as int,
+    @Id as varchar(33)
+)
+AS
+insert into BankAccountBalanceSnapshots
+    (Id, EntityId, AirslipUserType, AccountId, Balance, TimeStamp, Currency, UpdatedOn)
+select dbo.getId(), AB.EntityId, AB.AirslipUserType, AB.AccountId,
+       case AB.BalanceStatus when 1 then AB.Balance * -1 else AB.Balance end, AB.TimeStamp, AB.Currency,
+       dbo.round5min(DATEADD(ss, AB.TimeStamp/1000, '19700101'))
+from BankAccounts as a
+         join BankAccountBalances AB on a.Id = AB.AccountId
+where a.EntityId = @EntityId
+  and a.AirslipUserType = @AirslipUserType
+  and ab.Id = @Id
+order by AB.TimeStamp DESC
