c93aec31a1d8325b72eb391e119a3159b6986c06
Graham Whitehouse <83427700+gwairslip@users.noreply.github.com>
2022-02-17 11:53:38 +0000

    Add timestamp to feed from api 2 cart

--- src/Airslip.Analytics.Core/Models/Raw/Api2Cart/RawApi2CartAccountModel.cs
+++ src/Airslip.Analytics.Core/Models/Raw/Api2Cart/RawApi2CartAccountModel.cs
@@ -16,4 +16,5 @@ public record RawApi2CartAccountModel
     public string AirslipApiKey { get; private set; } = string.Empty;
     public string Name { get; init; } = string.Empty;
     public bool SupportsWebhooks { get; set; }
+    public long TimeStamp { get; set; }
 }
