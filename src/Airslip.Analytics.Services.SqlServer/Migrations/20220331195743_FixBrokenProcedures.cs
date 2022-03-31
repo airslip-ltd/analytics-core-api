58702c3653d31f6549b2a142fd8e7cdead0c5d8e
Graham Whitehouse <gwhitehouse@airslip.com>
2022-03-31 21:03:47 +0100

    Fixed broken procedure

---
+++ src/Airslip.Analytics.Services.SqlServer/Migrations/20220331195743_FixBrokenProcedures.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Airslip.Analytics.Services.SqlServer.Migrations
+{
+    public partial class FixBrokenProcedures : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
