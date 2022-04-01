6f3d80989b452b55707b73c5736445a19a2df7e8
Graham Whitehouse <gwhitehouse@airslip.com>
2022-04-01 10:19:47 +0100

    Added heartbeat

---
+++ src/Airslip.Analytics.Api/Controllers/HeartbeatController.cs
@@ -0,0 +1,43 @@
+﻿using Airslip.Common.Monitoring.Interfaces;
+using Airslip.Common.Monitoring.Models;
+using Airslip.Common.Utilities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Airslip.Analytics.Api.Controllers
+{
+    [AllowAnonymous]
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Produces(Json.MediaType)]
+    [Route("v{version:apiVersion}/heartbeat")]
+    public class HeartbeatController : ControllerBase
+    {
+        private readonly IHealthCheckService _healthCheckService;
+
+        public HeartbeatController(IHealthCheckService healthCheckService)
+        {
+            _healthCheckService = healthCheckService;
+        }
+        
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [Route("ping")]
+        public IActionResult Ping()
+        {
+            return Ok();
+        }
+        
+        [HttpGet]
+        [ProducesResponseType(typeof(HealthCheckResponse), StatusCodes.Status200OK)]
+        [Route("health")]
+        public async Task<IActionResult> Health()
+        {
+            HealthCheckResponse heartbeatResponse = await _healthCheckService.CheckServices();
+            
+            return Ok(heartbeatResponse);
+        }
+    }
+}
