f26c0605e3bd19401886ea43d0c1c69855fdb820
Tom McDonough <tmcdonough@airslip.com>
2022-04-21 11:02:46 +0100

    Deleted old code for transactions commerce

--- src/Airslip.Analytics.Logic/Services.cs
+++ src/Airslip.Analytics.Logic/Services.cs
@@ -13,7 +13,6 @@ public static class Services
         services
             .AddScoped<IRelationshipService, RelationshipService>()
             .AddScoped<IBusinessService, BusinessService>()
-            .AddScoped<ITransactionService, TransactionService>()
             .AddScoped<IDashboardSnapshotService, DashboardSnapshotService>()
             .AddScoped<IRevenueAndRefundsService, RevenueAndRefundsService>()
             .AddScoped<IDebitsAndCreditsService, DebitsAndCreditsService>()
