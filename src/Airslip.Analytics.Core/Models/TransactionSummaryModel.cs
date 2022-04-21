74adab101b9e4f3ed880fb939d8d1d45763d2b5f
Tom McDonough <tmcdonough@airslip.com>
2022-04-21 11:02:46 +0100

    Deleted old code for transactions commerce

--- src/Airslip.Analytics.Core/Models/TransactionSummaryModel.cs
+++
@@ -1,4 +0,0 @@
-namespace Airslip.Analytics.Core.Models;
-
-public record TransactionSummaryModel(string Id, string Source, double Amount, 
-    string CurrencyCode, long CapturedDate, string Description);
