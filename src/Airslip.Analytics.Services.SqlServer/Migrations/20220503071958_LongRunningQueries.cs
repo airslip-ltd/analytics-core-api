3777f18fe3f50388ed95131023e7e1ac68676404
Graham Whitehouse <gwhitehouse@airslip.com>
2022-05-03 08:21:14 +0100

    Added index changes

---
+++ src/Airslip.Analytics.Services.SqlServer/Migrations/20220503071958_LongRunningQueries.cs
@@ -0,0 +1,54 @@
+﻿using Airslip.Analytics.Services.SqlServer.Extensions;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Airslip.Analytics.Services.SqlServer.Migrations
+{
+    public partial class LongRunningQueries : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_MerchantTransactions_Day_Month_Year_EntityId_AirslipUserType_IntegrationId",
+                table: "MerchantTransactions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_MerchantAccountMetricSnapshots_Day_Month_Year_EntityId_AirslipUserType_IntegrationId",
+                table: "MerchantAccountMetricSnapshots");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MerchantTransactions_Day_Month_Year_EntityId_AirslipUserType_IntegrationId_CurrencyCode",
+                table: "MerchantTransactions",
+                columns: new[] { "Day", "Month", "Year", "EntityId", "AirslipUserType", "IntegrationId", "CurrencyCode" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MerchantAccountMetricSnapshots_Day_Month_Year_EntityId_AirslipUserType_IntegrationId_CurrencyCode",
+                table: "MerchantAccountMetricSnapshots",
+                columns: new[] { "Day", "Month", "Year", "EntityId", "AirslipUserType", "IntegrationId", "CurrencyCode" });
+            
+            migrationBuilder.AddSqlFiles();
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_MerchantTransactions_Day_Month_Year_EntityId_AirslipUserType_IntegrationId_CurrencyCode",
+                table: "MerchantTransactions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_MerchantAccountMetricSnapshots_Day_Month_Year_EntityId_AirslipUserType_IntegrationId_CurrencyCode",
+                table: "MerchantAccountMetricSnapshots");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MerchantTransactions_Day_Month_Year_EntityId_AirslipUserType_IntegrationId",
+                table: "MerchantTransactions",
+                columns: new[] { "Day", "Month", "Year", "EntityId", "AirslipUserType", "IntegrationId" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MerchantAccountMetricSnapshots_Day_Month_Year_EntityId_AirslipUserType_IntegrationId",
+                table: "MerchantAccountMetricSnapshots",
+                columns: new[] { "Day", "Month", "Year", "EntityId", "AirslipUserType", "IntegrationId" });
+        }
+    }
+}
